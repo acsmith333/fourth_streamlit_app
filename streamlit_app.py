e946a7c530a332d80b022f1eae1612c601d8a4d1
acsmith333 <127986545+acsmith333@users.noreply.github.com>
2023-03-31 13:28:05 -0700

    Update streamlit_app.py
    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Moms Healthy Diner')
+'
+streamlit.header('Breakfast Menu')
+streamlit.text('Bacon & Eggs')
+streamlit.text('Eggs Benedict')
+streamlit.text('Spinach Omelet')
